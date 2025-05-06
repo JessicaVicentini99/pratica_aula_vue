515bd424b9097e9a63e95f4d9e6ed48a8a05cc81
Jessica Vicentini <jvicentini99@gmail.com>
2025-05-06 17:16:45 -0300

    projeto sala de aula

---
+++ src/components/Pratica.vue
@@ -0,0 +1,62 @@
+<script>
+export default {
+  name: 'Pratica',
+  props: {
+    mensagemPai: String,
+    numeroPai: Number
+  },
+  data() {
+    return {
+      msg: 'Hello World',
+      numero: 123,
+      contador: 0,
+      items: [1, 2, 3],
+      items_objetos: [
+        {id: 1, name: 'Joao'},
+        {id: 2, name: 'Maria'},
+        {id: 3, name: 'Jose'}
+      ]
+    }
+  },
+
+  methods: {
+    incrementarContador() {
+      this.contador++
+    }
+  }
+}
+</script>
+<template>
+  <div>
+    <h3>Este é um componente</h3>
+    <h2>{{ msg }}</h2>
+    <br>
+    <h2>O contador é: {{ contador }}</h2>
+    <button @click="incrementarContador">
+      Incrementar Codntador
+    </button>
+
+    <h1>{{ mensagemPai }}</h1>
+
+    <h1>{{ numeroPai }}</h1>
+
+    <h2>{{ items }}</h2>
+
+    <div v-for="item in items" :key="item">
+      <h2>Numero: {{ item }}</h2>
+    </div>
+
+    {{ items_objetos }}
+
+    <div v-for="item in items_objetos" :key="item.id">
+      ID: {{item.id}} - Nome: {{item.name}}
+    </div>
+
+    <div v-for="(item, posicao) in items_objetos" :key="item.id">
+      Indice: {{posicao}} - ID: {{item.id}} - Nome: {{item.name}}
+    </div>
+  </div>
+</template>
+<style>
+
+</style>
---
+++ src/views/PraticaView.vue
@@ -0,0 +1,25 @@
+<script>
+import Pratica from '@/components/Pratica.vue'
+
+export default {
+  components: {Pratica},
+  data(){
+    return {
+      msgPai: 'Essa é a mensagem do componente pai',
+    }
+  }
+}
+</script>
+<template>
+  <div>
+    <h1>Pagina de pratica das aulas</h1>
+    <b>:)</b>
+    <pratica
+      :mensagemPai="msgPai"
+      :numeroPai="15"
+    />
+  </div>
+</template>
+<style>
+
+</style>
